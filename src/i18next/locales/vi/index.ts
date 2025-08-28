7fc4586217363776034d0f104ea42c8d87fb8949
PhamGiaLuong <pham15032004@gmail.com>
2025-08-28 20:40:07 +0700

    feat - add not found page

--- src/i18next/locales/vi/index.ts
+++ src/i18next/locales/vi/index.ts
@@ -1,5 +1,7 @@
 import Header from './Header.json'
+import Errors from './Errors.json'
 
 export default {
   Header,
+  Errors,
 }
--- src/pages/errors/NotFound.tsx
+++ src/pages/errors/NotFound.tsx
@@ -1,5 +1,24 @@
+import BodyBox from '@/components/BodyBox'
+import theme from '@/styles/Theme'
+import { Typography } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+
 const NotFound = () => {
-  return <h2>404 - Not Found</h2>
+  const { t } = useTranslation()
+  return (
+    <BodyBox>
+      <Typography
+        variant="h1"
+        color={theme.palette.error.main}
+        textAlign={'center'}
+      >
+        {t('Errors:NotFound:title')}
+      </Typography>
+      <Typography variant="body1" textAlign={'center'}>
+        {t('Errors:NotFound:description')}
+      </Typography>
+    </BodyBox>
+  )
 }
 
 export default NotFound
